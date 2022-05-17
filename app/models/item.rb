33d290a83f22a0aecc1a1dfea33986ed41777c0a
CosmoBLACK <okito09.kiwami@gmail.com>
2022-05-17 08:57:52 +0000

    商品ページ作成中

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,16 @@ class Item < ApplicationRecord
   has_many :order_details
   has_many :orders, through: :order_details
   has_one_attached :item_image
+
+  def add_tax_price
+    (self.price * 1.10).round
+  end
+
+  def get_profile_image(width,height)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [width, height]).processed
+  end
 end
