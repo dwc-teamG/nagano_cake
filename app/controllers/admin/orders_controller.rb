c14f3904018dffc3b46057abedc8fee29be4b30b
Taaku0405 <rakosutaa@gmail.com>
2022-05-18 07:38:17 +0000

    カミナリ編集とアドミン側注文途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,11 @@
 class Admin::OrdersController < ApplicationController
+  
+  def index
+    @orders = Order.page(parms[:page])
+  end 
+  
+  def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+  end 
 end
