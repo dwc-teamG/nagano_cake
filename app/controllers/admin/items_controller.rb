c8f1e15b49ca475bac7aa3a6bf60ddd31f7c95f6
CosmoBLACK <okito09.kiwami@gmail.com>
2022-05-19 10:06:01 +0000

    商品ページほぼ完成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -30,7 +30,7 @@ class Admin::ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to admin_item_path(@item.id)
+      redirect_to admin_item_path
     else
       render :edit
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,17 @@ class Item < ApplicationRecord
   has_many :orders, through: :order_details
   has_one_attached :item_image
 
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :introduction, presence: true
   validates :price, presence: true
+
+  def get_item_image(width, height)
+    unless item_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      item_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    item_image.variant(resize_to_limit: [width, height]).processed
+  end
 
   #消費税計算用のメソッド
   def add_tax_price
