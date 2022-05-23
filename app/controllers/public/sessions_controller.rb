cdfa16c5cbbbf8b8c529bbf0db9a704efa803eb0
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-05-23 06:26:45 +0000

    退会機能追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,6 +3,7 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  before_action :customer_state, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -17,9 +18,26 @@ class Public::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
+  def after_sign_in_path_for(resource)
+    root_path
+  end
 
-  # protected
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
+  protected
+  # 退会しているかを判別するメソッド
+  def customer_state
+    ## 【処理内容1】入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## 【アカウントを取得できなかった場合、このメソッドを終了
+    return if !@customer
+    ## 【処理内容2】取得したアカウントのパスワードと入力されたパスワードが一致しているかを判別
+    if @customer.valid_password?(params[:customer][:password]) && (@customer.deleted_flag == true)
+      redirect_to new_customer_registration_path
+    end
+  end
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
