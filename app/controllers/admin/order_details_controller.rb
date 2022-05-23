cdce1231b2f435319ed2cae19eb18a358c1e341d
Taaku0405 <rakosutaa@gmail.com>
2022-05-23 07:35:09 +0000

    アドミン注文ステータス動作修正

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,21 +1,21 @@
 class Admin::OrderDetailsController < ApplicationController
 
-    def updte
-     @order_detail = OrderDetail.find(params[:id])
-     @order_detail.update(order_detail_parms)
-     if params[:order_detail][:production_status]=="製作中"
-        @order_detail.order.update(order_status: 2)
-     end
-     if params[:order_detail][:production_status]=="製作完了"
+  def update
+   @order_detail = OrderDetail.find(params[:id])
+   @order_detail.update(order_detail_params)
+   if params[:order_detail][:production_status]=="製作中"
+      @order_detail.order.update(order_status: 2)
+   end
+   if @order_detail.order.order_details==@order_detail.order.order_details.where(production_status:3)
+      params[:order_detail][:production_status]=="製作完了"
       @order_detail.order.update(order_status: 3)
-     end
-     redirect_to request.referer
-    end
+   end
+    redirect_to request.referer
+  end
 
-    private
-
-    def order_detail_params
-    params.require(:order_detail).permit(:production_status)
-    end
+  private
+  def order_detail_params
+   params.require(:order_detail).permit(:production_status)
+  end
 
 end
