f4c192435964e92de5608de6441fbf5b13807b21
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-05-24 10:31:21 +0000

    kaminari with bootstrap4

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,7 +2,7 @@ class Public::ItemsController < ApplicationController
   before_action :authenticate_customer!, only: [:show]
 
   def index
-    @items = Item.page(params[:page])
+    @items = Item.page(params[:page]).per(8)
     @genres = Genre.all
   end
 
