4f7eec338f8edc1c8cdf8f53e6b12f242d28f586
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-05-25 05:54:24 +0000

    モデルの記述修正

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -4,6 +4,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_up_path_for(resource)
+    my_page_customers_path
+  end
   # GET /resource/sign_up
   # def new
   #   super
