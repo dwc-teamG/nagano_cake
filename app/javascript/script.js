27c4823d3c35e7aa8dc8828ed721a582ed7dae34
Risa-Imai <101160553+Risa-Imai@users.noreply.github.com>
2022-06-08 03:16:03 +0000

    レイアウト微調整

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,43 +1,46 @@
 /*global $*/
-$(document).ready(function () {
-  $('.complete').hide().fadeIn(1000);
-});
-
-$(document).ready(function () {
-  $('.about').hide().fadeIn(1000);
-});
-
-$(document).ready(function() {
-    var rotate = $(".about_image");
-    var hover = false;
-    var roop;
-    var count = 0;
-
-    //回転させる
-    function start(){
-        roop = setInterval(function(){
-            count++;
-            if(count > 360){
-                count = 0;
-            }else{
-                rotate.css("transform", "rotate("+ count +"deg)");
-            }
-        }, 10);
-    }
-
-    //クリックされた時の処理
-    rotate.hover(function(){
-        toggle();
+document.addEventListener("turbolinks:load", function() {
+    $(document).ready(function () {
+      $('.complete').hide().fadeIn(1000);
     });
-
-    //ループ中かどうか判断する
-    function toggle(){
-        if(hover){
-            clearInterval(roop);
-            hover = false;
-        }else{
-            start();
-            hover = true;
+    
+    $(document).ready(function () {
+      $('.about').hide().fadeIn(1000);
+    });
+    
+    
+    $(document).ready(function() {
+        var rotate = $(".about_image");
+        var hover = false;
+        var roop;
+        var count = 0;
+    
+        //回転させる
+        function start(){
+            roop = setInterval(function(){
+                count++;
+                if(count > 360){
+                    count = 0;
+                }else{
+                    rotate.css("transform", "rotate("+ count +"deg)");
+                }
+            }, 10);
         }
-    }
+    
+        //クリックされた時の処理
+        rotate.hover(function(){
+            toggle();
+        });
+    
+        //ループ中かどうか判断する
+        function toggle(){
+            if(hover){
+                clearInterval(roop);
+                hover = false;
+            }else{
+                start();
+                hover = true;
+            }
+        }
+    })
 });
